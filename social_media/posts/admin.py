a2f934e922bb20e5f6a5d9ffca95c6a680018985
amin <aminmohammadlou99@gmail.com>
2021-09-16 17:55:12 +0430

    Create Post model and Make migrations

--- social_media/posts/admin.py
+++ social_media/posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'caption', 'author', 'location', 'likes_count', 'comments_count',
+                    'created_time', 'updated_time')
+
+    ordering = ('created_time',)
+    
--- social_media/posts/models.py
+++ social_media/posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Post(models.Model):
+    caption = models.TextField(_('Caption'), max_length=500, blank=True, null=False)
+    image = models.ImageField(_('Image'), upload_to='post_images', blank=False, editable=False)
+    author = models.ForeignKey(User, verbose_name=_('Author'), on_delete=models.CASCADE)
+    location = models.CharField(_('Location'), max_length=30, blank=True)
+    likes = models.ManyToManyField(User, verbose_name=_('Likes'), related_name='likers', blank=True, symmetrical=False)
+    likes_count = models.IntegerField(_('Likes Count'), default=0, blank=False, null=False)
+    comments_count = models.IntegerField(_('Comments Count'), default=0, blank=False, null=False)
+    created_time = models.DateTimeField(_('Created Time'), auto_now_add=True) 
+    updated_time = models.DateTimeField(_('Updated Time'), auto_now=True)
+
+    class Meta:
+        db_table = 'post'
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
+
+    def __str__(self):
+        return f"Post by {self.author}"
+
+ 

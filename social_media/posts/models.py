f35403396031d17474528b08d6141b0a707f5f47
amin <aminmohammadlou99@gmail.com>
2021-09-17 13:57:26 +0430

    Start users app and Make migrations

--- social_media/posts/models.py
+++ social_media/posts/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
 
 class Post(models.Model):
     caption = models.TextField(_('Caption'), max_length=500, blank=True, null=False)
     image = models.ImageField(_('Image'), upload_to='post_images', blank=False, editable=False)
-    author = models.ForeignKey(User, verbose_name=_('Author'), on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('Author'), on_delete=models.CASCADE)
     location = models.CharField(_('Location'), max_length=30, blank=True)
     created_time = models.DateTimeField(_('Created Time'), auto_now_add=True) 
     updated_time = models.DateTimeField(_('Updated Time'), auto_now=True)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, verbose_name=_('Likes'), related_name="likers")
 
     class Meta:
         db_table = 'post'
@@ -20,7 +23,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, verbose_name=_('Author'), on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_('Author'), on_delete=models.CASCADE)
     post = models.ForeignKey(Post, verbose_name=_('Post'), on_delete=models.CASCADE)
     message = models.TextField(_('Message'), max_length=500, blank=False, null=False)
     created_time = models.DateTimeField(_('Created Time'), auto_now_add=True) 
---
+++ social_media/users/models.py
@@ -0,0 +1,48 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, phone_number, password=None):
+        if username is None:
+            raise TypeError(_('Username is a required field'))
+
+        user = self.model(username=username, phone_number=phone_number)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, phone_number, password=None):
+        if password is None:
+            raise TypeError(_('Password is a required field'))
+
+        user = self.create_user(username, phone_number, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.is_verified = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=100, unique=True)
+    email = models.EmailField(_('email'), max_length=255, unique=True, blank=True)
+
+    phone_number_validator = RegexValidator(r'^(\+98|0)?9\d{9}$', message=_('Invalid phone number'))
+
+    phone_number = models.PositiveBigIntegerField(_('phone number'), unique=True, validators=[phone_number_validator])
+    is_verified = models.BooleanField(_('is verified'), default=False)
+    is_active = models.BooleanField(_('is active'), default=True)
+    is_staff = models.BooleanField(_('is staff'), default=False)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time'), auto_now=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['phone_number']
+
+    objects = UserManager()
+
+    def str(self):
+        return self.username

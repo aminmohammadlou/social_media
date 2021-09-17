6929e4087b248ba8e108409232a6f0ad135c897b
amin <aminmohammadlou99@gmail.com>
2021-09-17 18:38:33 +0430

    Create Register API View

--- social_media/users/views.py
+++ social_media/users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics, status
+from .serializers import RegistrationSerializer
+from rest_framework import permissions
+from .models import User
 
-# Create your views here.
+
+class RegisterAPIView(generics.GenericAPIView):
+    serializer_class = RegistrationSerializer
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = {
+                'success': 'Successfully Registered',
+                'username': user.username,
+            }
+        
+        return Response(data=data, status=status.HTTP_201_CREATED)

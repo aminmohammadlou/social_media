5d2be894a7ebd81ba68a85d2b8bdfaba366203e0
amin <aminmohammadlou99@gmail.com>
2021-09-20 22:49:23 +0430

    Refactor Post Endpoint URL

--- social_media/posts/urls.py
+++ social_media/posts/urls.py
@@ -6,12 +6,12 @@ from .views import (PublishAPIView, LikeAPIView, CommentAPIView,
 app_name = 'posts'
 
 urlpatterns = [
-    path('publish/', PublishAPIView.as_view(), name='publish'),
+    path('publish', PublishAPIView.as_view(), name='publish'),
     path('like/<int:pk>', LikeAPIView.as_view(), name='like'),
     path('comment/<int:pk>', CommentAPIView.as_view(), name='comment'),
-    path('posts-list/', PostListAPIView.as_view(),name='posts_list'),
-    path('post-detail/<int:pk>/', PostDetailAPIView.as_view(), name='post_detail'),
-    path('delete-post/<int:pk>/', DeletePostAPIVIew.as_view(), name='delete-post'),
+    path('posts-list', PostListAPIView.as_view(),name='posts_list'),
+    path('post-detail/<int:pk>', PostDetailAPIView.as_view(), name='post_detail'),
+    path('delete-post/<int:pk>', DeletePostAPIVIew.as_view(), name='delete-post'),
     path('delete-comment/<int:pk>', DeleteCommentAPIView.as_view(), name='delete_comment'),
     path('<int:pk>/likes', PostLikesAPIView.as_view(), name='likes'),
     path('<int:pk>/comments', PostCommentsAPIiew.as_view(), name='comments'),

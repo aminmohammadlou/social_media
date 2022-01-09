63155db65e9a7eaa61958319b10d219644aebfdb
Amin Mohammadlou <aminmohammadlou99@gmail.com>
2022-01-09 13:57:25 +0330

    Limit the access of admin to notification admin tab

--- social_media/notifications/admin.py
+++ social_media/notifications/admin.py
@@ -8,4 +8,12 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display = ('id', 'from_user', 'post', 'comment', 'to_user', 'action', 'created_time', 'updated_time')
     ordering = ['-created_time']
     search_fields = ['from_user__username', 'to_user__username']
-    raw_id_fields = ['from_user', 'post', 'to_user']
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
